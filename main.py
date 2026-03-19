77b1a002466841a389ed80cc39072eb5a041f347
Luffnich <andrewwilliamnicholas@gmail.com>
2026-03-19 13:38:29 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+import os
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot {bot.user} sudah ON!')
+
+@bot.event
+async def on_member_join(member):
+    # Bot cari channel bernama 'selamat-datang'
+    channel = discord.utils.get(member.guild.text_channels, name='selamat-datang')
+    if channel:
+        await channel.send(f"Halo {member.mention}, selamat datang! 🎉")
+
+bot.run(os.getenv('DISCORD_TOKEN'))
